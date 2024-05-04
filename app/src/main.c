d00183b150166d3fca5efcdabe51593489709fd7
Nick Elsmore <nicholaselsmore@gmail.com>
2024-05-03 21:08:53 -0700

    turn on LED at start

--- app/src/main.c
+++ app/src/main.c
@@ -14,31 +14,36 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
+static int initialize() {
+	int ret;
+
+	if (!gpio_is_ready_dt(&led))
+		return -EAGAIN;
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0)
+		return ret;
+
+	gpio_pin_set_dt(&led, 1);
+
+	load_driver_init();
+
+	return 0;
+}
+
 int main(void)
 {
 	int ret;
 
 	printk("Toadly Control %s\n", APP_VERSION_STRING);
 
-	load_driver_init();
-
-#if 0
-	if (!device_is_ready(led.port)) {
-		printk("led not ready\n");
-		return;
-	}
-
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	ret = initialize();
 	if (ret < 0) {
-		printk("unable to configure led\n");
-		return;
+		printk("failed to initialize: %d\n", ret);
+		return ret;
 	}
 
-	while (1) {
-		gpio_pin_toggle_dt(&led);
-		k_sleep(K_MSEC(200));
-	}
-#endif
+	while (1);
 
 	return 0;
 }
